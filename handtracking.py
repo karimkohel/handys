cd6f6e65bc23d71ada8314438a3101228859a13e
karim kohel <kareemkohel@gmail.com>
2022-07-03 22:45:52 +0200

    started shapping the file to be a module

--- handtracking.py
+++ handtracking.py
@@ -4,34 +4,42 @@ import mediapipe as mp
 handsMp = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
+hands = handsMp.Hands()
 
-cap = cv2.VideoCapture(0)
-with handsMp.Hands() as hands:
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            continue
 
-        h,w,c = image.shape
+def getPoint(point, image):
+    h, w, c = image.shape
 
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = hands.process(image)
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
 
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    image, 
-                    hand_landmarks, 
-                    handsMp.HAND_CONNECTIONS, 
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style()
-                )
-            # Flip the image horizontally for a selfie-view display.
-        cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-        if cv2.waitKey(1) & 0xFF == 27:
-                break
-cap.release()
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            pointxy = hand_landmarks.landmark[point]
+            cx, cy = int(pointxy.x*w), int(pointxy.y*h)
+            print(cx, cy)
+
+
+def main():
+
+    cap = cv2.VideoCapture(0)
+    with handsMp.Hands() as hands:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                continue
+
+            getPoint(0, image)
+
+                # Flip the image horizontally for a selfie-view display.
+            cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+            if cv2.waitKey(1) & 0xFF == 27:
+                    break
+    cap.release()
+
+if __name__ == "__main__":
+    main()
