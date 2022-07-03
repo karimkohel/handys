6dd57653e3698d37c6f9e74c546a0bf7a7d08074
karim kohel <kareemkohel@gmail.com>
2022-07-03 04:12:55 +0200

    started htm with mediapipe

---
+++ handtracking.py
@@ -0,0 +1,14 @@
+import cv2
+import mediapipe as mp
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cam.read()
+    cv2.imshow("Cam", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+cam.release()
