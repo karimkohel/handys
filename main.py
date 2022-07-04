7146ff41eaa8d65553fb8956a18e29035c2053d9
karim kohel <kareemkohel@gmail.com>
2022-07-04 22:22:54 +0200

    started off main module and tested distance between fingers

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from turtle import distance
 import cv2
+from handtracking import HandDetector
 
 cam = cv2.VideoCapture(0)
+handler = HandDetector()
 
 while True:
     ret, frame = cam.read()
     cv2.imshow("Cam", frame)
+
+    handsPoints = handler.getPoint((4,8), frame)
+
+    if handsPoints:
+        indexFingerPoint = handsPoints[0][0]
+        thumbFingerPoint = handsPoints[0][1]
+        distanceBetweenFingers = abs(indexFingerPoint[0]-thumbFingerPoint[0]) + abs(indexFingerPoint[1]-thumbFingerPoint[1])
+
 
     if cv2.waitKey(1) == ord('q'):
         break
